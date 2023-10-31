8ec4b198bf3d7cabc3283953b62905ec9a8ca6b1
oopall <opalamsalem@hilma.tech>
2023-10-31 17:03:31 +0200

    nothing new

--- js/database.js
+++ js/database.js
@@ -1,24 +1,26 @@
 class DataBase{
     constructor() {
-        this.users = [{id: 1, username: 'talya', password: 'talya123', phoneNumber: '093248029', countContacts: 1}];
-        this.contacts = [{userid: 1, contactList: [{id: 1, name: 'lskfjd', number: '43948'}]}];
         this.countUsers = 1;
+        this.users = [new User('talya', 'talya123', '087873682', this.countUsers)];
+        this.contacts = [new Contacts(this.countUsers)];
     };
 
     addUser(username, password, phoneNumber) {
         this.countUsers++;
-        this.users.push({'id': this.countUsers,'username': username, 'password': password, 'phoneNumber': phoneNumber});
+        this.users.push(new User(username, password, phoneNumber, this.countUsers));
         this.updateUsers();
     }
 
     getUserContacts(userid) {
+        debugger;
         let userC;
         for (let contact of this.contacts) {
+            console.log(contact);
             if (contact.userid === userid) {
                 userC = contact.contactList;
             }
         }
-        return userC; // if user not found, will return false
+        return userC || false; // if user not found, will return false
     }
 
     getContact(userid, contactid) {
@@ -35,17 +37,28 @@ class DataBase{
     }
 
     addContact(userid, cName, cNumber) {
+        let user=this.getUser(userid);
         let userC = this.getUserContacts(userid);
+        console.log(userC);
         if(userC) {
-            this.countContacts++;
-            userC.push({id: this.countContacts, cName: cName, cNumber: cNumber});
-            console.log(userC)
+            user.countContacts++;
+            userC.push({id: user.countContacts, cName: cName, cNumber: cNumber});
+            console.log(userC);
             this.contacts[this.getContactsIndex(userid)].contactList = userC;
             this.updateContacts();
             return this.getContact(userid, this.countContacts);
         } else {
             return false;
         }
+    }
+
+    getUser(userid) {
+        for(let user of this.users){
+            if (user.id === userid){
+                return user;
+            }
+        }
+        return false;
     }
 
     getContactsIndex(userId) {
@@ -67,5 +80,3 @@ class DataBase{
 
 const db = new DataBase();
 db.addContact(1, 'fdggsdf', '034958304');
-// console.log(db.getContactsIndex(1));
-console.log()
